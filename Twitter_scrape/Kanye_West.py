7d9d067c588603a6133ac79cd52fbfc2832710ea
derekYankie <derek.afriyie@icloud.com>
2016-09-04 23:25:26 -0400

    get both twitter handle and stats

--- Twitter_scrape/Kanye_West.py
+++ Twitter_scrape/Kanye_West.py
@@ -8,8 +8,16 @@ thepage = urllib2.urlopen(theurl)
 soup = BeautifulSoup(thepage, "html.parser")
 
 #Get Kanye West's twitter handle
-print (soup.title.text)
+#print (soup.title.text)
 
-#Get Profile stats: tweets following, followers, and likes
+''' #Get Profile stats: tweets following, followers, and likes
 print(soup.findAll('span',{"class":"ProfileNav-label"}))
-print(soup.findAll('span',{"class":"ProfileNav-value"}))
+print(soup.findAll('span',{"class":"ProfileNav-value"}))'''
+
+#Get both: Twitter handle and profile stats
+person = soup.title.text
+prof_stats = ""
+for labels in soup.findAll('a',{"class":"ProfileNav-stat"}):
+    prof_stats = prof_stats + "," + labels.text
+    print person + prof_stats
+
